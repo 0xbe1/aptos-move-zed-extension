7ee9cfabf2dd6bd7f2bcdc7cbac3270f816eb446
Greg Nazario <greg@gnazar.io>
2026-05-07 22:07:19 -0700

    refactor: convert to stateful extension struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 use zed_extension_api::{self as zed, Result};
 
-struct AptosMoveExtension;
+struct AptosMoveExtension {
+    cached_binary_path: Option<String>,
+}
 
 impl zed::Extension for AptosMoveExtension {
     fn new() -> Self {
-        Self
+        Self {
+            cached_binary_path: None,
+        }
     }
 
     fn language_server_command(
@@ -12,10 +16,9 @@ impl zed::Extension for AptosMoveExtension {
         _language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        // Look for aptos-language-server in PATH
         let server_path = worktree
             .which("aptos-language-server")
-            .ok_or_else(|| "aptos-language-server not found in PATH. Please install it with: cargo install --git https://github.com/aptos-labs/move-vscode-extension.git aptos-language-server")?;
+            .ok_or_else(|| "aptos-language-server not found in PATH. Install: cargo install --git https://github.com/aptos-labs/move-vscode-extension.git aptos-language-server")?;
 
         Ok(zed::Command {
             command: server_path,
